b2fd0d9ccf19499ae27564d85f5dc1a1993b1ac1
Lesley Keyes <lesleyjkeyes@gmail.com>
2023-03-09 21:44:57 -0600

    finished basic filters

--- playdateapi/views/pet_interest.py
+++ playdateapi/views/pet_interest.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from playdateapi.models import Pet, Interest, PetInterest
+from playdateapi.models import Pet, Interest, PetInterest, User
 
 class PetInterestView(ViewSet):
     """Pet Interest View"""
@@ -16,9 +16,19 @@ class PetInterestView(ViewSet):
 
     def list(self, request):
         pet_interests = PetInterest.objects.all()
+        
         pet_id = self.request.query_params.get("pet_id", None)
         if pet_id is not None:
             pet_interests = pet_interests.filter(pet_id=pet_id)
+        
+        pets = self.request.query_params.get("pets", None) 
+        if pets is not None:
+            pet_interests = pet_interests = PetInterest.objects.raw("select * from playdateapi_petinterest pi group by pt.pet_id")
+            
+        pet_interest_id = self.request.query_params.get("pet_interest_id", None)
+        if pet_interest_id is not None:
+            pet_interests = pet_interests.filter(pet_interest_id=pet_interest_id)
+        
         serializer = PetInterestSerializer(pet_interests, many=True)
         return Response(serializer.data)
 
@@ -36,9 +46,26 @@ class PetInterestView(ViewSet):
         pet_interest.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
 
+class UserSerializer(serializers.ModelSerializer):
+    """serializer for trips"""
+    class Meta:
+        model = User
+        depth = 1
+        fields = ('uid', 'id', 'first_name', 'last_name', 'about', 'profile_image', 'email', 'city', 'state', 'country')
+        
+class PetSerializer(serializers.ModelSerializer):
+    """serializer for pets"""
+    owner = UserSerializer()
+    class Meta:
+        model = Pet
+        depth = 1
+        fields = ('id', 'name', 'about', 'profile_image', 'breed', 'owner')
 class PetInterestSerializer(serializers.ModelSerializer):
     """serializer for Pet Interests"""
+    pet = PetSerializer
     class Meta:
         model = PetInterest
-        depth = 1
+        depth = 2
         fields = ('id', 'pet', 'pet_interest')
+        
+        
